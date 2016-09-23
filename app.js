b96f77815903ee4d337f7730d5b0d62ecf8022e4
Ray Richardson <raymerlinrichardson@gmail.com>
2016-09-22 18:06:18 -0700

    Note titles now bound with changes

--- app.js
+++ app.js
@@ -1,115 +1,115 @@
 var main = function() {
-"use strict";
+  "use strict";
 
-var current;
-var notes_default = [
-{"title": "Note", "text": "Test", "color": "red"},
-{"title": "Note again", "text": "Another note", "color": "blue"},
-{"title": "Note more", "text": "One more note", "color": "yellow"}];
+  var current;
+  var notes_default = [
+    {"title": "Note", "text": "Test", "color": "red"},
+    {"title": "Note again", "text": "Another note", "color": "blue"},
+    {"title": "Note more", "text": "One more note", "color": "yellow"}];
 
-var notes = [];
-var note = function(){
-this.title = "New note";
-this.text = "Add text here";
-this.color = "white";
-}
-var init = function() {
-  if (localStorage.getItem("notes")){
-    notes = JSON.parse(localStorage.getItem("notes"));
-    /*for(var i in notes) {
-      console.log(notes[i].title + " " + notes[i].text + " " + notes[i].color);
-    }*/
-  }
-  showNotes();
-  clearNote();
-};
-var showNotes = function(){
-  var output = "";
-  if (notes.length > 0) {
-    localStorage.setItem("notes", JSON.stringify(notes));
-  } else {
-    localStorage.removeItem("notes");
-  }
-  for(var item in notes){
-    output += "<input type='text' value='" + notes[item].title + "'>";
-  }
-  //console.log(output);
-  document.getElementById("notelist").innerHTML = output;
-  $("#notelist input").each(function(i){
-    $(this).on("click",{x:i}, function(event){
-      //console.log("Item " + event.data.x + " selected");
-      current = event.data.x;
-      $("#notelist input").each(function(i){
-        $(this).css("background-color", "gray");
+  var notes = [];
+  var note = function(){
+    this.title = "New note";
+    this.text = "Add text here";
+    this.color = "white";
+  };
+  var init = function() {
+    if (localStorage.getItem("notes")){
+      notes = JSON.parse(localStorage.getItem("notes"));
+    }
+    showNotes();
+    clearNote();
+  };
+  var showNotes = function(){
+    var output = "";
+    if (notes.length > 0) {
+      localStorage.setItem("notes", JSON.stringify(notes));
+    } else {
+      localStorage.removeItem("notes");
+    }
+    for(var item in notes){
+      output += "<input type='text' value='" + notes[item].title + "'>";
+    }
+    document.getElementById("notelist").innerHTML = output;
+    $("#notelist input").each(function(i){
+      $(this).on("click",{x:i}, function(event){
+        current = event.data.x;
+        $("#notelist input").each(function(i){
+          $(this).css("background-color", "gray");
+        });
+        $(this).css("background-color", "white");
+        showNote();
       });
-      $(this).css("background-color", "white");
-      showNote();
+      $(this).on("keypress",function(event){
+        notes[current].title = $(this).val() + String.fromCharCode(event.which);
+      });
+
     });
+  };
+
+  var showNote = function(){
+    document.getElementById("content").value = notes[current].text;
+    document.getElementById("content").style.backgroundColor = notes[current].color;
+    $("#note").show();
+  }
+
+  $("#clear").on("click", function(event) {
+    notes = [];
+    localStorage.removeItem("notes");
+    showNotes();
+    clearNote();
   });
-};
 
-var showNote = function(){
-  document.getElementById("content").value = notes[current].text;
-  document.getElementById("content").style.backgroundColor = notes[current].color;
-  $("#note").show();
-}
+  $("#default").on("click", function(event) {
+    notes = notes_default;
+    localStorage.removeItem("notes");
+    showNotes();
+  });
 
-$("#clear").on("click", function(event) {
-  notes = [];
-  localStorage.removeItem("notes");
+  var clearNote = function(){
+    $("#note").hide();
+  }
+
+  $("#content").on("keypress", function(event) {
+    notes[current].text = $(this).val() + String.fromCharCode(event.which);
+    //console.log("note " + current + ": " + notes[current].text);
+  });
+
+
+  // Add button: add a new note
+  $("#add").on("click", function(event) {
+  notes.push(new note());
+  current = notes.length - 1;
+  //console.log("New note added with index " + current);
   showNotes();
-  clearNote();
-});
-
-$("#default").on("click", function(event) {
-  notes = notes_default;
-  localStorage.removeItem("notes");
-  showNotes();
-});
-
-var clearNote = function(){
-  $("#note").hide();
-}
-
-$("#content").on("keypress", function(event) {
-  notes[current].text = $(this).val() + String.fromCharCode(event.which);
-  //console.log("note " + current + ": " + notes[current].text);
-});
-
-// Add button: add a new note
-$("#add").on("click", function(event) {
-notes.push(new note());
-current = notes.length - 1;
-//console.log("New note added with index " + current);
-showNotes();
-$("#notelist input:last").css("background-color", "white");
-showNote();
-});
-
-// Delete button
-$("#del").on("click", function(event) {
-  notes.splice(current, 1);
-  showNotes();
-  clearNote();
-});
-
-var changeColor = function(c) {
-  notes[current].color = c;
+  $("#notelist input:last").css("background-color", "white");
   showNote();
-}
-// Color buttons: change the note color
-$("#r").on("click", function(event) {
-  changeColor("red");
-});
-$("#b").on("click", function(event) {
-  changeColor("blue");
-});
-$("#y").on("click", function(event) {
-  changeColor("yellow");
-});
-$("#w").on("click", function(event) {
-  changeColor("white");
-});
-init();
+  });
+
+  // Delete button
+  $("#del").on("click", function(event) {
+    notes.splice(current, 1);
+    showNotes();
+    clearNote();
+  });
+
+  var changeColor = function(c) {
+    notes[current].color = c;
+    showNote();
+  }
+  // Color buttons: change the note color
+  $("#r").on("click", function(event) {
+    changeColor("red");
+  });
+  $("#b").on("click", function(event) {
+    changeColor("blue");
+  });
+  $("#y").on("click", function(event) {
+    changeColor("yellow");
+  });
+  $("#w").on("click", function(event) {
+    changeColor("white");
+  });
+  init();
 }
 $(document).ready(main);
